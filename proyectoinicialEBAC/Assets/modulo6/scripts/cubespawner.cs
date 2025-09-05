39f6516c22aabe60c487ff57c2dd6199d1fc15ce
SEBAS-111 <sebasholalego587@gmail.com>
2025-09-04 20:48:04 -0600

    se agrego archivos
    -se agrego el archivo cubespawner

---
+++ proyectoinicialEBAC/Assets/modulo6/scripts/cubespawner.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubespawner
+{
+
+    //start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //update is called once per frame
+    void update()
+    {
+
+    }
+}
