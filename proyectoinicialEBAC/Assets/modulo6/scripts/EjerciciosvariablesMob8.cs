387e99885190d263cb0abc50f38427f522a03934
SEBAS-111 <sebasholalego587@gmail.com>
2025-09-30 21:43:15 -0600

    - modulo 8
    - se agregaron archivos y se modificaron algunos

---
+++ proyectoinicialEBAC/Assets/modulo6/scripts/EjerciciosvariablesMob8.cs
@@ -0,0 +1,56 @@
+using System.Drawing;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+public class EjerciciosvariablesMob8 : MonoBehaviour
+{
+    short mishort = 0;
+    int miint = 0;
+    private Renderer objectRenderer;
+    public Color coloresPar = Color.white;
+    public Color coloresNon = Color.black;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float a, b;
+        float f;
+        a = 10;
+        b = 5;
+        f = a + b;
+        Debug.Log("el valor de f es:" + f);
+
+        objectRenderer = GetComponent<Renderer>();
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mishort += 3;
+        Debug.Log(mishort);
+
+    }
+    void FixedUpdate()
+    {
+        float f;
+        f = 1;
+        f *= 2;
+
+
+        int e, i;
+        e = 10;
+        i = 5;
+        miint = Random.Range(e, i);
+        if (miint % 2 == 0)
+        {
+            Debug.Log("el numero " + miint + "es Par");
+            objectRenderer.material.color = coloresPar;
+        }
+        else
+        {
+            Debug.Log("el numero " + miint + "es non");
+            objectRenderer.material.color = coloresNon;
+        }
+    }
+}
